3a614e6c916a3fd49ca8cfa123a6d2bea285a94f
Nhan Do <trongnhan19081994@gmail.com>
2021-08-08 22:39:45 +0700

    CRUD columns and cards

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,14 +1,18 @@
 import { ColumnModel } from '*/models/column.model'
 import { BoardModel } from '*/models/board.model'
+import { CardModel } from '*/models/card.model'
 
 const createNew = async (data) => {
     try {
+        //transaction mongodb
         const newColumn = await ColumnModel.createNew(data)
 
         //Update columnOrder Array in board collection
         const idNewColumn = newColumn.insertedId
         const result = await ColumnModel.getDataNewColumn(idNewColumn)
         await BoardModel.pushColumnOrder(result.boardId, idNewColumn.toString())
+
+        newColumn.cards = []
 
         return newColumn
     } catch (error) {
@@ -22,8 +26,17 @@ const update = async (id, data) => {
             ...data,
             updatedAt: Date.now()
         }
-        const result = await ColumnModel.update(id, updateData)
-        return result
+        if (updateData._id) delete updateData._id
+        if (updateData.cards) delete updateData.cards
+
+        const updatedColum = await ColumnModel.update(id, updateData)
+        const resultUpdatedColum = await ColumnModel.getDataNewColumn(updatedColum._id)
+        if (resultUpdatedColum._destroy) {
+            //delete many cards in this column
+            CardModel.deleteMany(resultUpdatedColum.cardOrder)
+        }
+
+        return resultUpdatedColum
     } catch (error) {
         throw new Error(error)
     }
